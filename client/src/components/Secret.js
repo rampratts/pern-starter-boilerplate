8ba69957d2905a8fc633ae02f34fab0f630cf00c
Ramiro Meneses <rampratts@gmail.com>
2020-07-21 12:34:36 -0300

    Account deletion

--- client/src/components/Secret.js
+++ client/src/components/Secret.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { UserContext } from './UserContext';
+
 
 function Secret() {
+    const [isLogged, setIsLogged] = useContext(UserContext);
+    const history = useHistory();
+
+    const deleteAccount = async () => {
+        const response = await fetch('/api/users', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8',
+                'auth_token': localStorage.getItem('auth_token')
+            }
+        })
+        const { result } = await response.clone().json();
+
+        if(result) {
+            localStorage.removeItem('auth_token');
+            history.push('/');
+            setIsLogged(false);
+        }
+    }
+    
     return (
         <div>
             <h1>Psst, only authenticated users can see this 🤫</h1>
             <p>This is a secured route. Only users previously registered and logged in can access this.</p>
+            <button onClick={deleteAccount}>Delete my account</button>
         </div>
     )
 }
